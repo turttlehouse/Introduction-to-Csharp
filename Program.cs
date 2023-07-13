6fe36acaf7872008c277f1d68c880ec6e6ca8b6b
turttlehouse <buildandrun464@gmail.com>
2023-07-13 15:04:36 +0545

    printing namaste nepal in csharp code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Name
+{
+    class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Namaste Nepal");
+        }
+    }
+    
+}
